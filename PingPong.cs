16bd97c0a290c4718a1c7a801a87b5c64c14bb77
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-11 03:31:44 +0100

    fixing bug related to branching

--- PingPong.cs
+++ PingPong.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic; // as you can see, this one is greyed out, don
 // a Dictionary is capable of storing strings and integers at a time
 
 
-class PingPong{
-    static void Main(){
+class PingPong
+{
+    static void Main()
+    {
         // after our Main entrypoint, we now move on to writing our Ping Pong Application
 
         Console.WriteLine("Hello My Beautiful User, Welcome to the Dynasty's PING PONG Tournament");
-        Console.WriteLine("Please Enter any Number of your Choice");
+        Console.WriteLine("Please Enter a Number greater than 0");
 
         // saving user's number and turning it into an double; a double can account for oth integer and decimal numbers
 
@@ -19,51 +21,87 @@ class PingPong{
         string userNumberInString = Console.ReadLine();
 
         int userNumber = int.Parse(userNumberInString);
-        
-        
+
+
         // a new feature that will run the Main() method again if user inputs a number less than 1
-        if(userNumber < 1){
-            Main();
-        }
+        // if (userNumber < 1)
+        // {
+        //     Main();
+        // }
 
         // running a for loop to show users all numbers less than the njumber user enterred 
-            // this message shouldn't be added to the forloop, it keeps showing it for every iteration of the loop and that is very Ugly
+        // this message shouldn't be added to the forloop, it keeps showing it for every iteration of the loop and that is very Ugly
 
-            Console.WriteLine("Here are the numbers less than " + userNumber);
+        Console.WriteLine("Here are the numbers less than " + userNumber);
 
         for (int lesserNumber = 1; lesserNumber <= userNumber; lesserNumber++)
-        
+
         {
-            Console.WriteLine(lesserNumber);
-            
+            // Console.WriteLine(lesserNumber); ... This is not needed, our forloop prints out the lesser numbers anyway
+
+
             // running a branch to determine if we have a ping, or a pong, or a ping-pong
             // FOR Ping: I used the modulo operator here to determine if I can divide any of the lesserNumber by 3
-            if(lesserNumber % 3 == 0 && lesserNumber % 5 ==0){
+            if ((lesserNumber % 3 == 0 && lesserNumber % 5 == 0))
+            {
                 Console.WriteLine("Ping-Pong");
 
                 // string newLesserNumber = lesserNumber.ToString();
-                // // newLesserNumber.Replace("3", "Ping");
-
-                // Console.WriteLine(newLesserNumber);
-
-
-
-                // string pingString = lesserNumber.ToString();
-
-                // pingString.Replace("3", "Ping");
-                // pingString.Remove(1);
-
-                // Console.WriteLine("Ping");
-
-                // Console.WriteLine(pingString);
-                
             }
-            else{
+
+            else if(lesserNumber % 3 == 0){
+                Console.WriteLine("Ping");
+            }
+
+            else if(lesserNumber % 5 == 0){
+                Console.WriteLine("Pong");
+            }
+            else
+            {
                 Console.WriteLine(lesserNumber);
             }
         }
 
-        
-        
+        // Console.WriteLine("Enter a number greater than 0.");
+        // string stringNumber = Console.ReadLine();
+        // int inputtedNumber = int.Parse(stringNumber);
+        // Console.WriteLine("--------------------");
+        // for (int index = 1; index <= inputtedNumber; index++)
+        // {
+        //     if ((index % 3 == 0) && (index % 5 == 0))
+        //     {
+        //         Console.WriteLine("Ping-Pong");
+        //     }
+        //     else if (index % 5 == 0)
+        //     {
+        //         Console.WriteLine("Pong");
+        //     }
+        //     else if (index % 3 == 0)
+        //     {
+        //         Console.WriteLine("Ping");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine(index);
+        //     }
+        // }
+
     }
 }
+
+// CODES THAT WERE NOTNEEDED FOR APPLICATION
+
+// // newLesserNumber.Replace("3", "Ping");
+
+// Console.WriteLine(newLesserNumber);
+
+
+
+// string pingString = lesserNumber.ToString();
+
+// pingString.Replace("3", "Ping");
+// pingString.Remove(1);
+
+// Console.WriteLine("Ping");
+
+// Console.WriteLine(pingString);
